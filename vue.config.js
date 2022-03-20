1a968d2279649cdeed683a7de72b082c1c4d7c01
NeskoDorc <nebojsa85.milosevic@gmail.comm>
2022-03-20 19:49:42 +0100

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 
 module.exports = {
+
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/' + process.env.CI_PROJECT_NAME + '/'
+  : '/',
+
+
     css: {
       loaderOptions: {
     scss: {
